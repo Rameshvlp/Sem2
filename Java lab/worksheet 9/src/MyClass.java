062d6a2c9287cef226b8e27df41216db3f88a9c5
22mx113 <22mx113@MCA.PSG>
2023-04-18 15:45:51 +0530

    java

---
+++ Java lab/worksheet 9/src/MyClass.java
@@ -0,0 +1,17 @@
+public class MyClass {
+    static void myMethod1(){
+
+    }
+    static void myMethod2(){
+
+    }
+
+    static void myMethod3(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
